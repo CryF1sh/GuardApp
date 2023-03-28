f4224c2aa1068c0eaf38fe8eb94d04ae0df3c2b2
CryFish <glebbogomolov04@gmail.com>
2023-03-28 23:10:24 +0300

    Добавьте файлы проекта.

---
+++ GuardApp/Forms/AccessControl.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GuardApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AccessControl : Window
+    {
+        private DispatcherTimer _timer;
+        private int _count = 0;
+
+        private User _currentUser = new User();
+
+        public AccessControl(User user)
+        {
+            InitializeComponent();
+            DataContext = _currentUser;
+            tblSotrudnik.Text = user.SurName + " " + user.Name[0] + ". " + user.Patronymic[0] +".";
+            List<Gender> genders = GuardianEntities.GetContext().Genders.ToList();
+            cmbGender.ItemsSource = genders;
+        }
+        int chek = 0;
+       
+        private void butSave_Click(object sender, RoutedEventArgs e)
+        {
+            if ((cmbGender.SelectedItem == null) || (textSurName == null) || (textName == null) || (textPatronymic == null) || (textPost == null)) 
+            {
+                chek ++;
+                if (chek == 2)
+                {
+                    MessageBox.Show("Блокировка окна на 5 минут!");
+                    _timer = new DispatcherTimer();
+                    _timer.Interval = TimeSpan.FromSeconds(1);
+                    _timer.Tick += Timer_Tick;
+                    _timer.Start();
+                    chek = 0;
+                    textSurName.IsReadOnly = true;
+                    textPatronymic.IsReadOnly = true;
+                    textPost.IsReadOnly = true;
+                    textName.IsReadOnly = true;
+                    cmbGender.IsEditable = false;
+                    butClear.IsEnabled = false;
+                    butSave.IsEnabled = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Данные были добавлены!");
+                GuardianEntities.GetContext().Users.Add(_currentUser);
+                GuardianEntities.GetContext().SaveChanges();
+                ClearText();
+            }
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _count++;
+
+            if (_count == 360) // Блокировка окна после 5 минут
+            {
+                _timer.Stop(); // Остановка таймера
+                textSurName.IsReadOnly = false;
+                textPatronymic.IsReadOnly = false;
+                textPost.IsReadOnly = false;
+                textName.IsReadOnly = false;
+                cmbGender.IsEditable = true;
+                butClear.IsEnabled = true;
+                butSave.IsEnabled = true;
+            }
+        }
+
+        private void ClearText()
+        {
+            textSurName.Text = null;
+            textPatronymic.Text = null;
+            textPost.Text = null;
+            textName.Text = null;
+            cmbGender = null;
+        }
+        private void butClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearText();
+        }
+
+
+    }
+}
---
+++ GuardApp/Forms/LoginWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace GuardApp
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        GuardianEntities DB = new GuardianEntities();
+        public LoginWindow()
+        {
+            InitializeComponent();
+            CBTypeUser.ItemsSource = DB.TypeUsers.ToList();
+            CBTypeUser.SelectedItem = CBTypeUser.Items[0];
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string pass = textPassword.Text;
+            string log = textLogin.Text;
+            string sec = SecretWord.Text;
+
+            using (var db = new GuardianEntities())
+            {
+                
+                var login = db.Users.AsNoTracking().FirstOrDefault(u => u.Login == log);
+                var password = db.Users.AsNoTracking().FirstOrDefault(u => u.Password == pass);
+                var secretword = db.Users.AsNoTracking().FirstOrDefault(u => u.SecretWord == sec);
+
+                if ((login == null) || (password == null) || (secretword == null))
+                {
+                    MessageBox.Show("Данные введены неверно!");
+                    return;
+                }
+                else
+                {
+                    if (CBTypeUser.SelectedItem == CBTypeUser.Items[0])
+                    {
+                        AccessControl accessControl = new AccessControl(login);
+                        accessControl.Show();
+                        this.Close();
+                    }
+                }
+
+            }
+        }
+        private void ForgottenData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
